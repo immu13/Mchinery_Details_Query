d6a877342a36db56588ea876f3d181e157b5fe0e
immu13 <71899855+immu13@users.noreply.github.com>
2020-10-07 07:10:13 +0530

    Add files via upload

---
+++ MachineryDetailsQuery.sql
@@ -0,0 +1,9 @@
+
+create table All_Division_Machinery_Details (Division varchar(3), Machine_Name varchar(50), Machine_Code varchar(5), Capacity varchar(10), 
+No_of_Machines varchar(5), Machine_serial_No varchar(10), Purchased_Date Date, Supplier_Name varchar(50), Machine_ID int primary key identity (1,1))
+
+select * from All_Division_Machinery_Details
+
+
+
+
